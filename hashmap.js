e104cc81a58387bb672420a59ceb90d05fd8692d
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2025-12-29 09:17:24 -0600

    Create hash(key) method

--- hashmap.js
+++ hashmap.js
@@ -3,4 +3,17 @@ class HashMap {
         this.loadFactor = 0.75;
         this.capacity = 16;
     }
+
+    hash(key) {
+        let hashCode = 0;
+
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
+            hashCode = primeNumber * hashCode + key.charCodeAt(i);
+        }
+        return hashCode;
+    }
 }
+
+let hash = new HashMap;
+console.log(hash.hash("Frank"));
