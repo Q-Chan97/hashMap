b512a89b6f9fb0b6e106a39c01fc4b83b83db3c1
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2025-12-30 09:01:10 -0600

    Update with has examples

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ import { HashMap } from "./hashmap.js";
 
 const test = new HashMap();
 test.set("apple", "red")
-test.get("apple"); // Should return "red";
+test.get("apple"); // Should return "red"
+test.has("apple"); // Should return true
+test.has("Sinatra"); // Should return false
 
 console.log(test);
