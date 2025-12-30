9287c33f197b0557ad7ca9d7a49d1acb2c3310f3
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2025-12-30 08:55:07 -0600

    Update main w/ get example

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import { HashMap } from "./hashmap.js";
 
 const test = new HashMap();
-test.set('apple', 'red')
+test.set("apple", "red")
+test.get("apple"); // Should return "red";
 
 console.log(test);
