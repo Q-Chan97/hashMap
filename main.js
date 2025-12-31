d12fb3de921477a02799713eecbdb4ec306ae5fb
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2025-12-30 22:12:28 -0600

    Update main with remove example

--- main.js
+++ main.js
@@ -2,8 +2,12 @@ import { HashMap } from "./hashmap.js";
 
 const test = new HashMap();
 test.set("apple", "red")
+test.set("banana", "yellow");
+test.set("carrot", "orange");
 test.get("apple"); // Should return "red"
 test.has("apple"); // Should return true
 test.has("Sinatra"); // Should return false
+
+test.remove("carrot") // Test should only have apple and banana keys in it
 
 console.log(test);
